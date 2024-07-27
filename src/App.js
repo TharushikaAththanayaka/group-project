7c092edff13e3152c7a506adab9e42e8ac56dfb0
NadeeshaAbey <anadeesha9@gmail.com>
2024-07-27 10:30:42 +0530

    Revert "UserLogin and AdminLogin"
    This reverts commit 83f780a99590ce7dc90037b903776de093fc0d7e.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
-import AdminLoginPage from "./pages/AdminLoginPage.js";
-import UserLoginPage from './pages/UserLoginPage'
+import Homepage from "./pages/Homepage";
+import AddComplaint from "./pages/AddComplaint";
+import AboutUs from "./pages/AboutUs.js";
+import ContactUs from "./pages/ContactUs";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/add-complaint" element={<AddComplaint />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+      </Routes>
+      <Footer />
     </div>
     
   );
